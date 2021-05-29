9bd231d485598c4440a34c9e2c5945e8acc2cae6
Hesan Naveed <hesannaveed21@gmail.com>
2021-05-29 17:18:34 +0500

    Day3 & 4: Services Section, Custom PopUp, Portfolio Section backgrounf

--- myscripts.js
+++ myscripts.js
@@ -1,29 +1,37 @@
 $(document).ready(function () {
-console.log("e");
     // Immediately Invoked. 
     // This will make sure whenever screen reloaded
     (function(){
         var scroll = $(window).scrollTop();
-        // if($(window).width() > 992){
-            if (scroll > 70) {
-                $(".navbar").css("background" , "#131821")
-            }
-            else{
-                $(".navbar").css("background" , "transparent")
-            }
-        // }
+        if (scroll > 70) {
+            $(".navbar").css("background" , "#131821")
+        }
+        else{
+            $(".navbar").css("background" , "transparent")
+        }
+
+        
     })()
 
     $(window).scroll(function(){
         let scroll = $(window).scrollTop();
+        if (scroll > 100) {
+            $(".navbar").css("background" , "#131821")
 
-        // if($(window).width() > 992){
-            if (scroll > 100) {
-                $(".navbar").css("background" , "#131821")
+        }else{
+            $(".navbar").css("background" , "transparent")
+        }
 
-            }else{
-                $(".navbar").css("background" , "transparent")
-            }
-        // }
+        if(scroll == 0 || scroll < 500){
+            $('.nav-item a').removeClass('active');
+            $('#first-link').addClass('active');
+        }else{
+            $('#first-link').removeClass('active');
+        }
     })
+
+    $('.nav-item a').click(function(){
+        $('.nav-item a').removeClass('active');
+        $(this).addClass('active');
+    });
 });
